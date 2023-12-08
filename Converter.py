00aff40a8c4af5c9636b1bdbdb765771d3e55954
Матвей Коновалов <ok__@outlook.com>
2023-12-08 15:07:16 +0300

    Starting items are now properly generated, recorded and outputted

--- Converter.py
+++ Converter.py
@@ -1,8 +1,15 @@
+import json
+
 def DictToString(dict):
     returnStr = ''
     for key, value in dict.items():
-        if key != "username": returnStr += f"{key}: {value},\n"
+        returnStr += f"{key}: {value},\n"
 
     returnStr = returnStr.rstrip(",\n")
 
     return returnStr
+
+def FilteredDictToJson(dict, excluded):
+    filtered = {k: dict[k] for k in dict if k not in excluded}
+
+    return json.dumps(filtered)
--- GptCore.py
+++ GptCore.py
@@ -44,31 +44,54 @@ class GptGamemaster:
 
         return chat_completion.choices[0].message.content
     
-    def __addUsername(self, username, answer_json):
-        return {'username': username, **answer_json}
-
-    def CreateCharacter(self, username, introduction):
-        sysMsg = self.dbContext.read_record("Prompts", "key", "CreateCharacter")["prompt"]
-        answer = self.SendMessage(sysMsg, introduction, 0)
-        ansJson = JsonManager.ExtractJson(answer)
-        if ansJson['Race'] == "Unknown":
-            ansJson['Race'] = "Human"
-        newAnsJson = self.__addUsername(username, ansJson)
-        self.dbContext.create_record("characters", newAnsJson)
-
-        result = Converter.DictToString(newAnsJson)
-
-        return result
+    def __addSmthToJson(self, name, content, json):
+        if isinstance(json, (tuple, list)): return [{name: content, **item} for item in json]
+        return {name: content, **json}
+        
 
     def CreateWorld(self, username, setting):
         sysMsg = self.dbContext.read_record("Prompts", "key", "WorldCreation")["prompt"]
         answer = self.SendMessage(sysMsg, setting, 1)
         self.__saveCompressedInformation(username, "World", answer, "CompressWorldInfo")
         return answer
+    
+
+    def CreateCharacter(self, username, introduction):
+        sysMsg = self.dbContext.read_record("Prompts", "key", "CreateCharacter")["prompt"]
+        answer = self.SendMessage(sysMsg, introduction, 0)
+        ansDict = JsonManager.ExtractJson(answer)
+        if ansDict['Race'] == "Unknown":
+            ansDict['Race'] = "Human"
+        newansDict = self.__addSmthToJson('username', username, ansDict)
+        self.dbContext.create_record("characters", newansDict)
+
+        gear = self.__CreateStaringGear(username)
+
+        result = Converter.DictToString(ansDict)
+
+        return [result, gear]
+
+    
+    def __CreateStaringGear(self, username):
+        sysMsgs = [self.dbContext.read_record("Prompts", "key", "StartingGearCreation")["prompt"]]
+        worldInfo = self.dbContext.read_latest_record("world", "username", username)
+        character = self.dbContext.read_latest_record("characters", "username", username)
+        worldJson = Converter.FilteredDictToJson(worldInfo, ['id', 'username'])
+        characterJson = Converter.FilteredDictToJson(worldInfo, ['id', 'username'])
+        sysMsgs.append(worldJson)
+        gear = self.SendMessage(sysMsgs, characterJson, 0)
+        gearDict = JsonManager.ExtractJson(gear)
+        newansDict = self.__addSmthToJson('character_id', character["id"], gearDict)
+
+        for item in newansDict:
+            self.dbContext.create_record("items", item)
+
+        return gearDict
         
+
     def __saveCompressedInformation(self, username, tableName, text, format):
         sysMsg = self.dbContext.read_record("Prompts", "key", format)["prompt"]
         answer = self.SendMessage(sysMsg, text, 0)
-        ansJson = JsonManager.ExtractJson(answer)
-        newAnsJson = self.__addUsername(username, ansJson)
-        self.dbContext.create_record(tableName, newAnsJson)
+        ansDict = JsonManager.ExtractJson(answer)
+        newansDict = self.__addUsername(username, ansDict)
+        self.dbContext.create_record(tableName, newansDict)
